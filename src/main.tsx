d0c0342071c0d58af05ddcd0fe7b542ac6cedccf
tatchintate <florefoko67@gmail.com>
2026-08-17 11:12:27 +0100

    mise en place de la structure de Kaza rule

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import "@fontsource/sora/400.css";
+import "@fontsource/sora/600.css";
+import "@fontsource/sora/700.css";
+import "@fontsource/geist-sans/400.css";
+import "@fontsource/geist-sans/500.css";
+import "@fontsource/geist-sans/600.css";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
